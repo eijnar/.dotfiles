ac3e2c1b54aaf6aeaf6ad2becf9b048185d1f2f8
Johan Andersson <git@kaffesump.se>
2023-03-10 11:25:55 +0100

    Fixed issue with APT

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@
 check_dependencies(){
     #Declare list of dependencies
     declare -Ag deps=(
-        [tmux]='docker.io'
+        [tmux]='tmux'
     )
 
     #Declare list of package managers and their usages
     declare -Ag packman_list=(
         [pacman]='sudo pacman -Sy'
-        [apt]='echo "deb http://deb.debian.org/debian buster-backports main contrib non-free" > /etc/apt/sources.list.d/buster-backports.list; sudo apt update -y; sudo apt install -y'
+        [apt]='echo "sudo apt update -y; sudo apt install -y'
         [yum]='yum install -y epel-release; yum repolist -y; yum install -y')
 
     #Find the package manager on the system and install the package
